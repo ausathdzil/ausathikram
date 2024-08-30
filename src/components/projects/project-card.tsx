d1d21cc6f01d9e175db271a1a09c4b7c9977fadd
Ausath Abdi Dzil Ikram <147676922+ausathdzil@users.noreply.github.com>
2024-08-30 14:03:26 +0700

    Update projects card styling

--- src/components/projects/project-card.tsx
+++ src/components/projects/project-card.tsx
@@ -15,11 +15,11 @@ export default function ProjectCard({ project }: { project: Project }) {
       href={project.image ? `/projects/${project.slug}` : `${project.repo}`}
       target={project.image ? '_self' : '_blank'}
     >
-      <Card className="flex flex-col items-center border-none shadow-none hover:bg-accent-foreground/5 dark:hover:bg-accent/15 transition ease-in-out p-6 gap-4">
+      <Card className="text-muted-foreground hover:text-primary flex flex-col items-center border-none shadow-none hover:bg-accent-foreground/5 dark:hover:bg-accent/15 transition ease-in-out p-6 gap-4">
         {project.image && (
           <CardHeader className="relative w-[300px] h-[150px]">
             <Image
-              className="object-cover rounded-lg"
+              className="object-cover rounded-lg border-zinc-500 border-2"
               src={project.image}
               alt={project.title}
               quality={100}
@@ -27,7 +27,7 @@ export default function ProjectCard({ project }: { project: Project }) {
             />
           </CardHeader>
         )}
-        <CardContent className="text-muted-foreground hover:text-primary transition ease-in-out">
+        <CardContent className="">
           <CardTitle>{project.title}</CardTitle>
           <CardDescription>{project.description}</CardDescription>
         </CardContent>
