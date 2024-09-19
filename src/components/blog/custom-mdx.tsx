a5a78575a0e2663ad8e01b21b786b4d9d5346854
Ausath Abdi Dzil Ikram <147676922+ausathdzil@users.noreply.github.com>
2024-09-19 20:29:30 +0700

    Explicitly use any for now

--- src/components/blog/custom-mdx.tsx
+++ src/components/blog/custom-mdx.tsx
@@ -1,25 +1,25 @@
 import { MDXRemote } from 'next-mdx-remote/rsc';
 
 const components = {
-  h1: (props) => (
+  h1: (props: any) => (
     <h1
       className="text-4xl text-foreground font-bold"
       {...props}
     />
   ),
-  h2: (props) => (
+  h2: (props: any) => (
     <h2
       className="text-3xl text-foreground font-bold"
       {...props}
     />
   ),
-  p: (props) => (
+  p: (props: any) => (
     <p
       className="text-lg"
       {...props}
     />
   ),
-  strong: (props) => (
+  strong: (props: any) => (
     <strong
       className="text-foreground"
       {...props}
@@ -27,7 +27,7 @@ const components = {
   ),
 };
 
-export default function CustomMDX(props) {
+export default function CustomMDX(props: any) {
   return (
     <MDXRemote
       {...props}
