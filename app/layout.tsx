bd8a4c9dfc239ea6dab847514806e1958d80c715
Ausath Ikram <ausathdzil@gmail.com>
2026-02-07 17:15:28 +0700

    feat: add geist, remove google fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
+import { GeistMono } from 'geist/font/mono'
+import { GeistSans } from 'geist/font/sans'
 import type { Metadata } from 'next'
 
-import { geistMono, libreFranklin } from '@/lib/fonts'
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -27,10 +28,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${libreFranklin.variable} ${geistMono.variable} font-sans`}
-      >
-        <main className="prose prose-neutral mx-auto w-full prose-pre:bg-neutral-50 p-8 prose-blockquote:font-serif prose-code:font-mono prose-h1:font-semibold prose-h2:font-semibold prose-h3:font-semibold prose-pre:font-mono prose-a:text-blue-700 prose-blockquote:text-lg prose-h1:text-2xl prose-blockquote:not-italic md:px-0 md:py-16 prose-blockquote:[&>p:first-of-type::before]:content-[''] prose-blockquote:[&>p:last-of-type::after]:content-['']">
+      <body className={`${GeistSans.variable} ${GeistMono.variable} font-sans`}>
+        <main className="prose prose-neutral mx-auto w-full prose-pre:bg-neutral-50 p-8 prose-blockquote:font-serif prose-code:font-mono prose-h1:font-semibold prose-h2:font-semibold prose-h3:font-semibold prose-pre:font-mono prose-a:text-blue-700 prose-blockquote:text-lg prose-h1:text-2xl prose-blockquote:not-italic prose-code:before:content-[''] prose-code:after:content-[''] md:px-0 md:py-16 prose-blockquote:[&>p:first-of-type::before]:content-[''] prose-blockquote:[&>p:last-of-type::after]:content-['']">
           {children}
         </main>
         <Analytics />
