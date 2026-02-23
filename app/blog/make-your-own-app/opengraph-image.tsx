30983e84c692698d4483da6633583398aebd4f5a
Ausath Ikram <ausathdzil@gmail.com>
2026-02-23 23:17:18 +0700

    feat: add og image to new post

---
+++ app/blog/make-your-own-app/opengraph-image.tsx
@@ -0,0 +1,14 @@
+import { createOGImage, ogImageContentType, ogImageSize } from '@/components/og'
+import { getPostMetadata } from '@/lib/blog'
+
+const post = await getPostMetadata('make-your-own-app')
+
+export const alt = post.title
+export const size = ogImageSize
+export const contentType = ogImageContentType
+
+export default async function Image() {
+  return await createOGImage({
+    children: post.title,
+  })
+}
